49fca063960763d6a1bd9eca775b2a8c45983353
thenameisanzu <ansuvs047@gmail.com>
2026-01-09 09:18:55 +0530

    First API route added (/api/test)

--- Server/app.js
+++ Server/app.js
@@ -3,6 +3,9 @@ const app = express();
 
 app.use(express.json());
 
+const testRoutes = require("./routes/testRoutes");
+app.use("/api", testRoutes);
+
 app.get("/", (req, res) => {
   res.send("Backend working");
 });
