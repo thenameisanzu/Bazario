cbb0767559b5d01461239a3b91e7453c37281130
thenameisanzu <ansuvs047@gmail.com>
2026-01-11 08:41:55 +0530

    MongoDB connection configured

--- Server/routes/testRoutes.js
+++ Server/routes/testRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { testController } = require("../controllers/testController");
+const { testController } = require("../controllers/testControllers");
 
 router.get("/test", testController);
 
