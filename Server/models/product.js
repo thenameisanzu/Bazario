af765376423774bfa22ce41d1977edab0c4bef4e
thenameisanzu <ansuvs047@gmail.com>
2026-01-17 08:40:15 +0530

    Added Product model for shopping features

---
+++ Server/models/product.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    category: {
+      type: String,
+      required: true
+    },
+    brand: {
+      type: String
+    },
+    image: {
+      type: String,
+      required: true
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    rating: {
+      type: Number,
+      default: 0
+    },
+    numReviews: {
+      type: Number,
+      default: 0
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("Product", productSchema);
