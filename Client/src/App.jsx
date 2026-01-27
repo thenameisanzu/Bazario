81fa9dd6d5021f7e328c16074f3377353bb862a4
thenameisanzu <ansuvs047@gmail.com>
2026-01-28 01:32:50 +0530

    Set up frontend pages and routing structure

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,30 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Products from "./pages/Products";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+import Orders from "./pages/Orders";
+
 function App() {
-  const addToCart = async () => {
-    const token = localStorage.getItem("token");
-
-    console.log("TOKEN:", token);
-
-    const res = await fetch("http://localhost:5003/api/cart", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify({
-        productId: "696c4bd33dc417d6f5065d49",
-        quantity: 1,
-      }),
-    });
-
-    const data = await res.json();
-    console.log("CART RESPONSE:", data);
-  };
-
   return (
-    <div>
-      <h1>Test Cart</h1>
-      <button onClick={addToCart}>Add to Cart</button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/orders" element={<Orders />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
