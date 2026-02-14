68ed413bdbcd79fad69ae24a3312de0ed8b75183
thenameisanzu <ansuvs047@gmail.com>
2026-02-14 23:59:18 +0530

    Added src/context

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { CartProvider } from "./context/CartContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+<BrowserRouter>
+  <CartProvider>
     <App />
-  </BrowserRouter>
+  </CartProvider>
+</BrowserRouter>
 );
